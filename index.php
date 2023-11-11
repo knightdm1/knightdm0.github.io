62d55e72a725a9e7db7308e0b7e71a05bd08bdb1
knightdm1 <146425948+knightdm1@users.noreply.github.com>
2023-11-11 12:36:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 
-$to = "youremail@mail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$to = "danima5689dm@gmail.com";
+$subject = "Mail From knightdm";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . ";
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
